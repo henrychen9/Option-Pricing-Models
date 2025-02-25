63f3e3c6dc4a85385fe0f2ec69c7488dc6313bdf
ketchupovo <henrychen2027@u.northwestern.edu>
2025-02-25 03:07:27 -0600

    Force update

--- Black Scholes Call.py
+++ Black Scholes Call.py
@@ -1,5 +1,4 @@
-# Black Scholes calculator
-# Henry Chen
+# Black Scholes Call
 
 import numpy as np
 from scipy.stats import norm
@@ -11,12 +10,5 @@ def black_scholes_call(S, K, T, t, r, sigma):
 
     C = S * norm.cdf(d1) - K * np.exp(-r * (T - t)) * norm.cdf(d2)
     return C
-S = 100    # Stock price
-K = 100    # Strike price
-T = 1.0    # Expiration time (1 year)
-t = 0.0    # Current time
-r = 0.05   # Risk-free rate (5%)
-sigma = 0.2  # Volatility (20%)
 
-price = black_scholes_call(S, K, T, t, r, sigma)
-print("Black-Scholes Call Price:", price)
+"test"
